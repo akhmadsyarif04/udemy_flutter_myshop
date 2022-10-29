138dbd55ae916aaaa7d6971f98d602b278dfeec2
akhmadsyarif04 <akhmadsyarif04.arif@gmail.com>
2022-10-29 12:53:40 +0800

    Adding Navigation to the App, menambahkan halaman detail product dan navigasi ke halaman produk ketika diklik gambar product di product item

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import "package:flutter/material.dart";
 
+import "../screens/product_detail_screen.dart";
+
 class ProductItem extends StatelessWidget {
   final String id;
   final String title;
@@ -15,9 +17,16 @@ class ProductItem extends StatelessWidget {
         borderRadius: BorderRadius.circular(
             10), // wajib menggunakan ClipRRect untuk menambahkan border radius
         child: GridTile(
-          child: Image.network(
-            imageUrl,
-            fit: BoxFit.cover,
+          child: GestureDetector(
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (ctx) => ProductDetailScreen(title)),
+              );
+            },
+            child: Image.network(
+              imageUrl,
+              fit: BoxFit.cover,
+            ),
           ),
           footer: GridTileBar(
             backgroundColor: Colors.grey,
