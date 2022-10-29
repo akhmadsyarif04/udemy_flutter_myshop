6495f0dea96cc7e65f34a9eb288f7c5afed81003
akhmadsyarif04 <akhmadsyarif04.arif@gmail.com>
2022-10-29 12:25:34 +0800

    Styling & Theming the App. merubah warna dan border radius card product item, menambah font external

---
+++ lib/models/product.dart
@@ -0,0 +1,22 @@
+import "package:flutter/foundation.dart";
+
+class Product {
+  final String? id;
+  final String? title;
+  final String? description;
+  final double? price;
+  final String? imageUrl;
+  bool isFavorite;
+  /*
+  ? mengartikan nullable, atau null valid
+  */
+
+  Product({
+    @required this.id,
+    @required this.title,
+    @required this.description,
+    @required this.price,
+    @required this.imageUrl,
+    this.isFavorite = false,
+  });
+}
---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+import "package:flutter/material.dart";
+
+class ProductItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  ProductItem(this.id, this.title, this.imageUrl); // position argument
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+        borderRadius: BorderRadius.circular(
+            10), // wajib menggunakan ClipRRect untuk menambahkan border radius
+        child: GridTile(
+          child: Image.network(
+            imageUrl,
+            fit: BoxFit.cover,
+          ),
+          footer: GridTileBar(
+            backgroundColor: Colors.grey,
+            leading: IconButton(
+              icon: Icon(Icons.favorite),
+              onPressed: () {},
+              color: Theme.of(context).accentColor,
+            ),
+            title: Text(
+              title,
+              textAlign: TextAlign.center,
+            ),
+            trailing: IconButton(
+              icon: Icon(
+                Icons.shopping_cart,
+              ),
+              onPressed: () {},
+              color: Theme.of(context).accentColor,
+            ),
+          ),
+        ));
+  }
+}
