96ebdbeceef6017bfb9d137bcc3e64ada338bd48
akhmadsyarif04 <akhmadsyarif04.arif@gmail.com>
2022-10-29 22:36:52 +0800

    Listening Provider in Different Places & Ways, mencari detail data berdasarkan id dan penerapan logic pada file provider bukan pada file screens

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   // final String title;
@@ -13,9 +16,14 @@ class ProductDetailScreen extends StatelessWidget {
     if (route == null) return SizedBox.shrink();
     final productid = route.settings.arguments as String; // this is id
     // selanjutnya dapatkan semua data dari id yang didapat
-
+    final loadedProduct =
+        Provider.of<Products>(context, listen: false).findById(productid);
+    /*
+        listen: false = jika ada perubahan dari listen dan products data provider maka akan render bagian ini saja. bagus untuk digunakan dalam mendapatkan atau mencari data pada state management provider.
+        listen: true = maka semua yang memanggil provider products ketika ada perubahan akan dirender ulang. (default)
+        */
     return Scaffold(
-      appBar: AppBar(title: Text('title')),
+      appBar: AppBar(title: Text(loadedProduct.title.toString())),
     );
   }
 }
