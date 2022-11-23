0146a1054cfde50828868c4e6297f78be3d1bc98
akhmadsyarif04 <akhmadsyarif04.arif@gmail.com>
2022-11-23 17:47:57 +0800

    Making orders expandable & stateful widgets vs providers

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -38,7 +38,7 @@ class CartScreen extends StatelessWidget {
                     */
                     Chip(
                       label: Text(
-                        '\$${cart.totalAmount}',
+                        '\$${cart.totalAmount.toStringAsFixed(2)}',
                         style: TextStyle(
                           color: Colors.white,
                         ),
