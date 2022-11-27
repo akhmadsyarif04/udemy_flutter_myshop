b4bfb57c21c608a1e93d58f64fd0d4a235c54ece
akhmadsyarif04 <akhmadsyarif04.arif@gmail.com>
2022-11-27 14:08:24 +0800

    sending POST Request with package http

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,4 +1,7 @@
+import 'dart:convert'; // untuk convert data
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 import './product.dart';
 
@@ -66,6 +69,21 @@ class Products with ChangeNotifier {
   // }
 
   void addProduct(Product product) {
+    // begin send add data to firebase
+    // https://shop-app-flutter-472e2-default-rtdb.asia-southeast1.firebasedatabase.app/
+    final url = Uri.https(
+        'shop-app-flutter-472e2-default-rtdb.asia-southeast1.firebasedatabase.app',
+        '/products.json');
+    http.post(url,
+        body: json.encode({
+          'title': product.title,
+          'description': product.description,
+          'price': product.price,
+          'imageUrl': product.imageUrl,
+          'isFavorite': product.isFavorite
+        }));
+    // end send add data to firebase
+
     final newProduct = Product(
         id: DateTime.now().toString(),
         title: product.title,
