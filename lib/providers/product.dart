68901ddc8d9b30679d29d74be65b6bf1725b9f31
akhmadsyarif04 <akhmadsyarif04.arif@gmail.com>
2022-12-01 21:30:24 +0800

    Updating the Favorite Status Optimistically to firebase

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,6 @@
 import "package:flutter/foundation.dart";
+import "package:http/http.dart" as http;
+import "dart:convert";
 
 class Product with ChangeNotifier {
   final String? id;
@@ -20,8 +22,26 @@ class Product with ChangeNotifier {
     this.isFavorite = false,
   });
 
-  void toggleFavoritStatus() {
+  void _setFavoriteValue(bool newValue) {
+    isFavorite = newValue;
+    notifyListeners();
+  }
+
+  Future<void> toggleFavoritStatus() async {
+    final oldStatus = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners(); // agar provider mendengarkan bahwa ada perubahan pada data
+    final url = Uri.https(
+        'shop-app-flutter-472e2-default-rtdb.asia-southeast1.firebasedatabase.app',
+        '/products/$id.json');
+    try {
+      final response =
+          await http.patch(url, body: json.encode({'isFavorite': isFavorite}));
+      if (response.statusCode >= 400) {
+        _setFavoriteValue(oldStatus);
+      }
+    } catch (error) {
+      _setFavoriteValue(oldStatus);
+    }
   }
 }
